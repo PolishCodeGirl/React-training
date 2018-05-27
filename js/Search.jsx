e0126d4ea9b51877be00ef3fbd92e0fb7d693be3
Marzena Malachowska <marzena.malachowska@kreditech.com>
2018-05-27 10:43:54 +0200

    project setup

---
+++ js/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Search = () => (
+  <div>
+    <h1>Hi it is Search form the Moon</h1>
+    <Link to='/'>BACK</Link>
+  </div>
+)
+
+export default Search;
