e54d5fa8dcafdeadc363bdff6b721e357b07ae28
Marzena Malachowska <malachowska.marzena@gmail.com>
2018-05-28 07:21:34 +0200

    Add state to Search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const Search = () => (
-  <div className='search'>
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.title} show={show}/>)}
-    </div>
-  </div>
-)
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchValue: ''
+    }
+
+    this.handleSearchOutput = this.handleSearchOutput.bind(this)
+  }
+
+  handleSearchOutput (event) {
+    this.setState({
+      searchValue: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className='search'>
+        <header>
+          <h1>Shows!</h1>
+          <input
+            value={this.state.searchValue}
+            onChange={this.handleSearchOutput}
+            type='text'
+            placeholder='Search...'/>
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.title} show={show}/>)}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Search;
