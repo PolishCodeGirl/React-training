cad43f232d37f456cb8613e12b6cd11c5681b299
Marzena Malachowska <malachowska.marzena@gmail.com>
2018-07-05 22:06:39 +0200

    Add Details component and passing function to the Route

--- js/App.jsx
+++ js/App.jsx
@@ -1,8 +1,11 @@
 // @flow
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
+import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>
 
@@ -12,6 +15,10 @@ const App = () => (
       <Switch>
         <Route exact path='/' component={Landing}/>
         <Route path='/search' component={Search}/>
+        <Route path='/details/:id' component={ (props: { match: Match}) => {
+          const currentShow = preload.shows.find( show => props.match.params.id === show.imdbID)
+          return(<Details show={currentShow} {...props}/>)
+        }}/>
         <Route component={FourOhFour}/>
       </Switch>
     </div>
