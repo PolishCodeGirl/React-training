715398fad4f2992b321203452a8ab6845a3ca33a
Marzena Malachowska <malachowska.marzena@gmail.com>
2018-07-18 22:08:36 +0200

    Connect Redux to React

--- js/App.jsx
+++ js/App.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import type { Match } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -11,6 +13,7 @@ const FourOhFour = () => <h1>404</h1>
 
 const App = () => (
   <BrowserRouter>
+    <Provider store={store}>
     <div className='app'>
       <Switch>
         <Route exact path='/' component={Landing}/>
@@ -22,6 +25,7 @@ const App = () => (
         <Route component={FourOhFour}/>
       </Switch>
     </div>
+  </Provider>
   </BrowserRouter>
 )
 
--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,6 +1,6 @@
 import { SET_SEARCH_TERM } from './actions'
 
-export const setSearchTerm = (searchTerm) => ({
+export const setSearchTerm = (searchTerm) => ({ // eslint-disable-line import/prefer-default-export
   type: SET_SEARCH_TERM,
   payload: searchTerm
 })
