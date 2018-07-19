b93c12b0e9133e14a8b8c03fff10e5f8d530bd04
Marzena Malachowska <malachowska.marzena@gmail.com>
2018-07-19 08:52:44 +0200

    Dispatching Actions to Redux

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -2,17 +2,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { setSearchTerm } from './actionCreators';
 
 const mapStateToProps = state => ({
   searchTerm: state.searchTerm
 })
 
-const Landing = () => (
+const mapDispatchToProps = ( dispatch: Function ) => ({
+  handleSearchTermChange(event) {
+    dispatch(setSearchTerm(event.target.value))
+  }
+})
+
+const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
   <div className='landing'>
     <h1>Search video</h1>
-    <input type='text' placeholder='Search...'/>
+    <input value={props.searchTerm} onChange={props.handleSearchTermChange} type='text' placeholder='Search...'/>
     <Link to='/search'>or Browse All</Link>
   </div>
 )
 
-export default connect(mapStateToProps)(Landing);
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
